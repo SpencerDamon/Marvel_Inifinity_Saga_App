9e305e0ee2ed24164284f07b66909b044242cd1e
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-10 10:27:08 -0700

    Commit to save layout with constraint layout, prep to try frame layout

--- app/src/main/java/com/gmail/spencerdamon1974/spencerchallenge/MainActivity.kt
+++ app/src/main/java/com/gmail/spencerdamon1974/spencerchallenge/MainActivity.kt
@@ -3,11 +3,13 @@ package com.gmail.spencerdamon1974.spencerchallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
 import com.gmail.spencerdamon1974.spencerchallenge.data.remote.repository.MarvelMovieRepository
 import com.gmail.spencerdamon1974.spencerchallenge.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +24,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setupLoadingState()
         CoroutineScope(Dispatchers.IO).launch {
             val marvelMovies = marvelMovieRepository.getMarvelMovies().toList()
-            //marvelAdapter = MarvelAdapter(marvelMovies)
+            marvelAdapter = MarvelAdapter(marvelMovies)
             Log.d("Movies", marvelMovies.toString())
+            withContext(Dispatchers.Main) {
+                setupRecyclerView()
+                setupSuccessState()
+            }
         }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerview.adapter = marvelAdapter
+    }
+
+    private fun setupSuccessState() = with(binding) {
+        recyclerview.isVisible = true
+        progressLoading.isVisible = false
+    }
+
+
+    private fun setupLoadingState() = with(binding) {
+        recyclerview.isVisible = false
+        progressLoading.isVisible = true
     }
 }
