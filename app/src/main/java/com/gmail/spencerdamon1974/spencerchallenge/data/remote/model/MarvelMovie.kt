3ac4d2da9a3b32fcf31da9f9af9715d820fb7132
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-09 18:57:01 -0700

    Create Model Class to represent the Type of the ArrayList

---
+++ app/src/main/java/com/gmail/spencerdamon1974/spencerchallenge/data/remote/model/MarvelMovie.kt
@@ -0,0 +1,8 @@
+package com.gmail.spencerdamon1974.spencerchallenge.data.remote.model
+
+data class MarvelMovie(
+    val poster: Poster,
+    val title: Title,
+    val released: Released,
+    val genre: Genre
+)
