5af4c06dd21032ca6d70455a08d73ea6869f184f
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-15 16:14:56 -0700

    Add business logic for button click listeners to nav to MainActivity

--- app/src/main/java/com/gmail/spencerdamon1974/spencerchallenge/SecondActivity.kt
+++ app/src/main/java/com/gmail/spencerdamon1974/spencerchallenge/SecondActivity.kt
@@ -1,11 +1,30 @@
 package com.gmail.spencerdamon1974.spencerchallenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gmail.spencerdamon1974.spencerchallenge.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private val binding: ActivitySecondBinding by lazy {
+        ActivitySecondBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(binding.root)
+
+        setUpListeners()
     }
+
+    private fun setUpListeners() {
+        binding.btnTop.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+        }
+
+        binding.btnBottom.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+        }
+    }
+
 }
